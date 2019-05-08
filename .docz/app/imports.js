025d154416228dbc39e61b594801d47a5508e3e4
Aryan Jabbari <AryanJabbari@gmail.com>
2019-05-07 21:30:42 -0400

    Add to functional components page

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -3,6 +3,10 @@ export const imports = {
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-pages-index" */ 'src/pages/index.mdx'
     ),
+  'src/pages/examples/functional-components.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-pages-examples-functional-components" */ 'src/pages/examples/functional-components.mdx'
+    ),
   'src/pages/installation/create-react-app.mdx': () =>
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-pages-installation-create-react-app" */ 'src/pages/installation/create-react-app.mdx'
